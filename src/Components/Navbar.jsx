76bb9c97ebf722594945e538d6d528c06be76c19
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-07-31 22:55:32 +0600

    social link added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Navbar = () => {
 
@@ -44,9 +46,26 @@ const Navbar = () => {
 
             {/* Social Link */}
 
-          
+            <div className='hidden lg:flex flex-col py-4 fixed top-[35%] left-0'>
+                <ul>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                        <a className='flex justify-between items-center w-full text-gray-300 text-xl' href="/"> Linkedin<FaLinkedinIn size={30}/></a>
+                    </li>
+                    
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#3333]'>
+                        <a className='flex justify-between items-center w-full text-gray-300 text-xl' href="/"> Github<FaGithub size={30}/></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2c0]'>
+                        <a className='flex justify-between items-center w-full text-gray-300 text-xl' href="/"> Email<HiOutlineMail size={30}/></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                        <a className='flex justify-between items-center w-full text-gray-300 text-xl' href="/"> Resume<BsFillPersonLinesFill size={30}/></a>
+                    </li>
+                    
+                </ul>
+            </div>
 
-
+    
         </div>
     );
 };
