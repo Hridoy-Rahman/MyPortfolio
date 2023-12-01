fa07b214e2f3e39d1656e09875163f12bc75ede6
Hridoy Rahman <hridoyrahman715@gmail.com>
2023-12-01 11:16:25 +0600

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -107,7 +107,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300 text-xl"
-              href="https://drive.google.com/file/d/1aW1hRQxrtaybVeFl97XK7mAvkH5rk2l6/view"
+              href="https://drive.google.com/file/d/1w7H7dCjzJXuifSYkwMnanbvWrQZzpZL3/view?usp=sharing"
             >
               {" "}
               Resume
