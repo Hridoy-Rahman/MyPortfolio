dc3fa8c93daf14ee86437b3c380c4abfe34e595c
Hridoy Rahman <hridoyrahman715@gmail.com>
2023-12-05 10:25:27 +0600

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,10 +7,9 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-pink-600'>Hi, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ffff]'>Hridoy Rahman</h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Full Stack Developer</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a Front-End Developer</h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>
-                    I'm a full-stack developer specializing in building (and occasionally
-                    designing) exceptional digital experiences. Currently, I'm focused on
+                    I'm a front-end developer specializing in building exceptional digital experiences. Currently, I'm focused on
                     building responsive full-stack web applications.
                 </p>
                 <div>
